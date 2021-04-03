493a0c413c4f76d66255ee0880e7bca4336dd104
Ubuntu <ubuntu@ip-172-31-20-128.us-east-2.compute.internal>
2021-04-03 13:52:22 +0000

    CSVエクスポート実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
+require "csv"
+CSV_COLUMNS = %w[name age height]
+
 class User < ApplicationRecord
+    def self.import_csv(file:)
+        list = []
+        CSV.foreach(file.path, headers: true) do |row|
+            list << row.to_h.slice(*CSV_COLUMNS)
+        end
+        User.import!(list)
+    end
+    
+    def self.generate_csv
+        CSV.generate do |csv|
+            csv << CSV_COLUMNS
+            all.each do |user|
+                csv << CSV_COLUMNS.map { |col| user.send(col) }
+            end
+        end
+    end
 end
